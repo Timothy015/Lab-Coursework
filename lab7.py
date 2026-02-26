5754584b1696557a961ed18f58431fa01ff62c03
Timothy <Timothy.L9915@outlook.com>
2026-02-26 10:37:47 -0800

    Created a function that prints province details from the dictionary

--- lab7.py
+++ lab7.py
@@ -45,20 +45,23 @@ def display_league():
 
 
 def get_total_population(this_dict):
-    """Gets the total population
-    :param this_dict is a dictionary"""
+    """Calculates and displays the total population of all provinces.
+    :param this_dict: Dictionary containing province data
+    :return: Sum of all provinces
+    """
     total = 0
     for province in this_dict.values():
         total += province["population"]
-        print(f"The total population of Canada is {total}")
-        return
+    print(f"The total population of Canada is {total}")
+    return total
 
 
 def get_capital_city(province_name, this_dict):
-    """Retrieves the capital city of the province
+    """Retrieves and displays the capital city of a given province.
     :param province_name: Name of the province
     :param this_dict: Dictionary containing province data
-    :return: Name of capital city of province"""
+    :return: Capital city of the province
+    """
     if province_name in this_dict:
         capital = this_dict[province_name]["capital"]
         print(f"The capital city of {province_name} is {capital}")
@@ -68,10 +71,11 @@ def get_capital_city(province_name, this_dict):
 
 
 def get_largest_city(province_name, this_dict):
-    """Retrieves the largest city of province
+    """Retrieves and displays the largest city of a given province.
     :param province_name: Name of the province
     :param this_dict: Dictionary containing province data
-    :return: Name of largest city in province"""
+    :return: Largest city in province
+    """
     if province_name in this_dict:
         largest = this_dict[province_name]["largest"]
         print(f"The largest city of {province_name} is {largest}")
@@ -81,16 +85,17 @@ def get_largest_city(province_name, this_dict):
 
 
 def get_smallest_province(this_dict):
-    """Retrieves the smallest province
+    """Determines and displays the province with the smallest population.
     :param this_dict: Dictionary containing province data
-    :return: Name of the smallest province in Canada"""
+    :return: Name of the smallest province in Canada
+    """
     smallest_province = None
     smallest_pop = None
 
     for province, value in this_dict.items():
-        # First iteration
         population = value["population"]
-        # Update if current population is smaller
+
+        # Update if current population is smaller than previous smallest
         if smallest_pop is None or population < smallest_pop:
             smallest_province = province
             smallest_pop = population
@@ -99,21 +104,42 @@ def get_smallest_province(this_dict):
 
 
 def get_largest_province(this_dict):
-    """Retrieves the largest province
+    """Determines and displays the province with the largest population.
     :param this_dict: Dictionary containing province data
-    :return: Name of the largest province"""
+    :return: Name of the largest province
+    """
     largest_province = None
     largest_pop = None
 
     for province, value in this_dict.items():
-        # First iteration
         population = value["population"]
-        # Update if current population is bigger
+
+        # Update if current population is bigger than previous
         if largest_pop is None or population > largest_pop:
             largest_province = province
             largest_pop = population
     print(f"The largest province in Canada is {largest_province}")
     return
+
+
+def print_province_details(province_name, this_dict):
+    """Displays formatted details about a province.
+    :param province_name: Name of a province
+    :param this_dict: Dictionary containing province data
+    """
+
+    if province_name in this_dict:
+        pop = this_dict[province_name]["population"]
+        capital = this_dict[province_name]["capital"]
+        large = this_dict[province_name]["largest"]
+
+        # Check if largest city is different from capital
+        if large != capital:
+            print(f"{province_name} has a populate of {pop} whose {capital} and largest city is {large}")
+
+        # If largest city and capital are the same
+        elif large == capital:
+            print(f"{province_name} has a populate of {pop} whose capital and largest city is {large}")
 
 
 def main():
@@ -138,9 +164,11 @@ def main():
 
     get_total_population(canada)
     get_capital_city("british columbia", canada)
-    get_largest_city("ontario", canada)
+    get_largest_city("quebec", canada)
     get_smallest_province(canada)
     get_largest_province(canada)
+    print_province_details("manitoba", canada)
+    print_province_details("british columbia", canada)
 
 
 if __name__ == "__main__":
